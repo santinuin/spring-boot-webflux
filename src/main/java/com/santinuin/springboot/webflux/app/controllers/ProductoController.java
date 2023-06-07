9ea1bfaeceb7ae2ec39d12f31cc7fa951dfd93fb
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-06-06 21:28:28 -0300

    Contrapresion

--- src/main/java/com/santinuin/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/santinuin/springboot/webflux/app/controllers/ProductoController.java
@@ -2,11 +2,16 @@ package com.santinuin.springboot.webflux.app.controllers;
 
 import com.santinuin.springboot.webflux.app.models.dao.ProductoDao;
 import com.santinuin.springboot.webflux.app.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductoController {
@@ -14,12 +19,58 @@ public class ProductoController {
     @Autowired
     private ProductoDao dao;
 
+    public static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+
+
     @GetMapping({"/listar", "/"})
     public String listar(Model model){
-        Flux<Producto> productos = dao.findAll();
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
+    };
+
+    @GetMapping("/listar-datadriver")
+    public String listarDataDriver(Model model){
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).delayElements(Duration.ofSeconds(1));
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
+        model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar";
+    };
+
+    @GetMapping("/listar-full")
+    public String listarFull(Model model){
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).repeat(5000);
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar";
+    };
+
+    @GetMapping("/listar-chunked")
+    public String listarChunked(Model model){
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).repeat(5000);
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar-chunked";
     };
 }
